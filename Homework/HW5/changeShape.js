e9b01bc5d623d5451d4ed9c2b6157ce1a2e41f6e
Derekwang2002 <80444343+Derekwang2002@users.noreply.github.com>
2023-06-06 19:09:29 +0800

    0606-19：09

---
+++ Homework/HW5/changeShape.js
@@ -0,0 +1,16 @@
+$(function(){
+    var classCircle = 'rounded-circle image-fluid'; //need these classes to make it a circle
+    $('#convert').click(function(){
+        if($('img').hasClass(classCircle)){
+            $('img.card-img-top').removeClass(classCircle);
+            $('img.pin').addClass('rounded-start').removeClass('rounded-circle pin');
+        }
+        else{
+            $('img.card-img-top').addClass(classCircle);
+            $('img.rounded-start').removeClass('rounded-start').addClass('rounded-circle pin'); 
+            //use pin class to spefify later
+            //$('img.rounded-start') img has a little conflict: they already have class image-fluid, so we donnot need consider it.
+            //$('img.rounded-start') also has class rounded-start, which can not exist simultaniously with class rounded-circle, so we have to remove it at first.
+        }
+    })
+})
---
+++ Homework/HW5/uniform.js
@@ -0,0 +1,3 @@
+!(function(){
+    $('p.title').addClass('fs-sm-12 fs-md-30 fs-lg-80');
+})
